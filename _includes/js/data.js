61d76b923f807c80ee2ad9e526572c28edb006de
Alex Barth <alex@developmentseed.org>
2012-01-18 19:08:39 -0500

    This is our data control structure.

---
+++ _includes/js/data.js
@@ -0,0 +1,147 @@
+
+var dataCtrl = [
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2009'
+    },
+    {
+        'label': 'Feb 2009'
+    },
+    {
+        'label': 'Jan 2011'
+    },
+    {
+        'label': 'Feb 2011'
+    },
+    {
+        'label': 'Jan 2011'
+    },
+    {
+        'label': 'Feb 2011'
+    },
+    {
+        'label': 'Jul 2011'
+    },
+    {
+        'label': 'Aug 2011'
+    },
+    {
+        'label': 'Sep 2011'
+    },
+    {
+        'label': 'Oct 2011'
+    },
+    {
+        'label': 'Nov 2011'
+    },
+    {
+        'label': 'Dec 2011'
+    }
+];
