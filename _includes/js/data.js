41b5aba7a432ba490c1d9175bbd8f39ec04b1a80
David Johnson <johnson@developmentseed.org>
2012-01-19 15:21:14 -0500

    fixed formatting on js

--- _includes/js/data.js
+++ _includes/js/data.js
@@ -22,97 +22,91 @@ var dataCtrl = [
         'label': 'Jan 2009'
     },
     {
+        'label': 'Feb 20010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
+        'label': 'Feb 2010'
+    },                    
+    {                     
+        'label': 'Jan 2010'
+    },                    
+    {                     
         'label': 'Feb 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Jan 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Feb 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Jan 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Feb 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Jan 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Feb 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Jan 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Feb 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Jan 2009'
-    },
-    {
+    },                    
+    {                     
         'label': 'Feb 2009'
+    },                    
+    {                     
+        'label': ' 2009'
+    },                    
+    {                     
+        'label': 'Oct 2010'
+    },                    
+    {                     
+        'label': 'Nov 2010'
     },
     {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
-    },
-    {
-        'label': 'Jan 2009'
-    },
-    {
-        'label': 'Feb 2009'
+        'label': 'Dec 2010'
     },
     {
         'label': 'Jan 2011'
@@ -121,10 +115,16 @@ var dataCtrl = [
         'label': 'Feb 2011'
     },
     {
-        'label': 'Jan 2011'
+        'label': 'Mar 2011'
     },
     {
-        'label': 'Feb 2011'
+        'label': 'Apr 2011'
+    },
+    {
+        'label': 'May 2011'
+    },
+    {
+        'label': 'Jun 2011'
     },
     {
         'label': 'Jul 2011'
