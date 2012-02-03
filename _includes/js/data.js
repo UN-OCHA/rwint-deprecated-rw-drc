0047913ae538ae7ec1d8fee77bcbcf2fe37ac679
David Johnson <johnson@developmentseed.org>
2012-02-02 19:09:33 -0500

    Removing array and function to practice merge conflicts

--- _includes/js/data.js
+++ _includes/js/data.js
@@ -1,10 +1,4 @@
 var Layers = function() {
-    this.active = {
-        sec: true,
-        lra: true,
-        ret: true,
-		idp: true
-    };
     this.layerCtrl = [
     {
         'month': 'Jan 2010',
@@ -220,13 +214,6 @@ var Layers = function() {
 Layers.prototype.current = function() {
     if (!this.layerCtrl[this.pos]) return;
     return this.filter(this.layerCtrl[this.pos].layers).join(',');
-};
-
-Layers.prototype.filter = function(layers) {
-    var active = this.active;
-    return _.filter(layers, function(v, k) {
-        return active[k];
-    });
 };
 
 Layers.prototype.month = function() {
