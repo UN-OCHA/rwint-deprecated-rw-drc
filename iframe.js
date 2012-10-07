74c6f00a807df16b86952a70e21dac35f902cd8d
davejohn <johnson@mapbox.com>
2012-10-07 18:26:02 -0400

    removing unecessary files from master

--- iframe.js
+++
@@ -1,9 +0,0 @@
----
----
-
-;{% include js/libs/jquery.min.js %}
-;{% include js/libs/underscore.js %}
-;{% include js/libs/dragdealer.js %}
-;{% include js/data.js %}
-;{% include js/iframe.js %}
-
